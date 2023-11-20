06b2d3c9e1824d14ec67308ac2c23e92a15ee45a
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2023-11-20 17:27:51 +0100

    Initial Commit

---
+++ SwiftSonarWave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSonarWave
+//
+//  Created by The Chainbreaker on 20.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSonarWave/SwiftSonarWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSonarWaveApp.swift
+//  SwiftSonarWave
+//
+//  Created by The Chainbreaker on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSonarWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
