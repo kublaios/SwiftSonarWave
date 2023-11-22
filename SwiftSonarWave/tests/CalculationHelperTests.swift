5e1baa6586e0bebbbe86389285fa47a72d59bb47
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2023-11-22 14:28:16 +0100

    Add tests for old methods.

--- SwiftSonarWave/tests/CalculationHelperTests.swift
+++ SwiftSonarWave/tests/CalculationHelperTests.swift
@@ -14,6 +14,17 @@ final class CalculationHelperTests: XCTestCase {
         XCTAssertEqual(CalculationHelper().squareRoot(value), 2.0)
     }
 
+    func testSquare() {
+        let value = 4.0
+        XCTAssertEqual(CalculationHelper().square(value), 16.0)
+    }
+
+    func testDistance() {
+        let pointA = CGPoint(x: 0.0, y: 0.0)
+        let pointB = CGPoint(x: 3.0, y: 4.0)
+        XCTAssertEqual(CalculationHelper().distance(from: pointA, to: pointB), 5.0)
+    }
+
     func testCube() {
         let value = 4.0
         XCTAssertEqual(CalculationHelper().cube(value), 64.0)
