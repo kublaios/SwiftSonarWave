8cac28f9ceace027b670351fb56c6e1210096456
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2023-11-23 22:58:51 +0100

    Add more methods and tests, use xcresultparser.

--- SwiftSonarWave/CalculationHelper.swift
+++ SwiftSonarWave/CalculationHelper.swift
@@ -19,4 +19,33 @@ struct CalculationHelper {
     func distance(from pointA: CGPoint, to pointB: CGPoint) -> Double {
         squareRoot(square(pointA.x - pointB.x) + square(pointA.y - pointB.y))
     }
+
+    func cube(_ value: Double) -> Double {
+        value * value * value
+    }
+
+    func power(_ base: Double, exponent: Int) -> Double {
+        pow(base, Double(exponent))
+    }
+
+    func factorial(_ value: UInt) -> UInt {
+        if value == 0 {
+            return 1
+        }
+        return value * factorial(value - 1)
+    }
+
+    func average(_ values: [Double]) -> Double {
+        guard !values.isEmpty else { return 0.0 }
+        let sum = values.reduce(0, +)
+        return sum / Double(values.count)
+    }
+
+    func percentage(_ part: Double, of total: Double) -> Double {
+        (part / total) * 100
+    }
+
+    func linearInterpolation(from start: Double, to end: Double, progress: Double) -> Double {
+        start + (end - start) * progress
+    }
 }
--- SwiftSonarWave/tests/CalculationHelperTests.swift
+++ SwiftSonarWave/tests/CalculationHelperTests.swift
@@ -13,4 +13,26 @@ final class CalculationHelperTests: XCTestCase {
         let value = 4.0
         XCTAssertEqual(CalculationHelper().squareRoot(value), 2.0)
     }
+
+    func testSquare() {
+        let value = 4.0
+        XCTAssertEqual(CalculationHelper().square(value), 16.0)
+    }
+
+    func testDistance() {
+        let pointA = CGPoint(x: 0.0, y: 0.0)
+        let pointB = CGPoint(x: 3.0, y: 4.0)
+        XCTAssertEqual(CalculationHelper().distance(from: pointA, to: pointB), 5.0)
+    }
+
+    func testCube() {
+        let value = 4.0
+        XCTAssertEqual(CalculationHelper().cube(value), 64.0)
+    }
+
+    func testPower() {
+        let base = 2.0
+        let exponent = 3
+        XCTAssertEqual(CalculationHelper().power(base, exponent: exponent), 8.0)
+    }
 }
